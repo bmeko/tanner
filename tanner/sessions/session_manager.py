1cb1d2e4acebbdfe72d1ddbdd982e81145ec6c07
johnbonjohn <111438682+johnbonjohn@users.noreply.github.com>
2022-08-18 20:14:39 +0300

    Update session_manager.py

--- tanner/sessions/session_manager.py
+++ tanner/sessions/session_manager.py
@@ -89,6 +89,7 @@ class SessionManager:
 
     async def delete_session(self, sess, redis_client):
         print("redis client place")
+        print(redis_client)
         await sess.remove_associated_db()
         if sess.associated_env is not None:
             await sess.remove_associated_env()
