d14ca9486bb2328052572aa3fe5e52205c7f434e
johnbonjohn <111438682+johnbonjohn@users.noreply.github.com>
2022-08-25 13:58:58 +0300

    Update inter.py

--- tanner/inter.py
+++ tanner/inter.py
@@ -12,13 +12,14 @@ async def ge(ids):
 	print(ids)
 	r= await redis_client.RedisClient.get_redis_client()
 	y= await r.get(id)
-	session= json.loads(y)
-	result = await SessionAnalyzer.create_stats(session, r)
-	print(result)
+	#session= json.loads(y)
+	#result = await SessionAnalyzer.create_stats(session, r)
+	print(y)
 
 def st(ids):
 	print("getting the data as test")
 	asyncio.run(tes())
 	print("get func")
 	asyncio.run(ge(ids))
+st("jojo")
 
