1f6a543b9af0c05cacdddebbf24e20baaea192a5
johnbonjohn <111438682+johnbonjohn@users.noreply.github.com>
2022-08-25 13:01:34 +0300

    Update inter.py

--- tanner/inter.py
+++ tanner/inter.py
@@ -17,8 +17,8 @@ async def ge(ids):
 	print(result)
 
 def st(ids):
-	print("getting the data as test")
 	if __name__=="__main__":
+		print("getting the data as test")
 		asyncio.run(tes())
 		asyncio.run(ge(ids))
 
